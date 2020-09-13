e7c93ece30ec0d88cc32e1e1cb08ab866bc8dd72
rokuro3 <mylabosango@gmail.com>
2020-09-13 20:19:41 +0900

    Initialize repository

---
+++ AB/test1.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string concat(string a, string b){
+  return a + b;
+}
+
+int main(){
+  using vi = vector<int>;
+  using vvi = vector<vi>;
+
+  int N = 10, M = 20;
+  vvi data(N,vi(M));
+}
---
+++ EX23/test1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() { 
+    map<string, int > score;
+    score["Alice"] = 100;
+    score["Bob"] = 89;
+    score["Charlie"] = 95;
+
+    cout << score.size() << endl;
+    
+
+ }
